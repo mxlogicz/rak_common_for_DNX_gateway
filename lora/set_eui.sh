a3c1ade657dc13ada7b1df773a66334512187ee6
Stefan <91331807+mxlogicz@users.noreply.github.com>
2021-09-26 09:39:44 +0200

    Update set_eui.sh

--- lora/set_eui.sh
+++ lora/set_eui.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 GATEWAY_EUI=""
-if [ ! -e "/opt/ttn-gateway/packet_forwarder/lora_pkt_fwd/local_conf.json" ]; then
+if [ ! -e "/opt/lora-gateway/packet_forwarder/lora_pkt_fwd/local_conf.json" ]; then
         GATEWAY_EUI_NIC="eth0"
         if [[ `grep "$GATEWAY_EUI_NIC" /proc/net/dev` == "" ]]; then
             GATEWAY_EUI_NIC="wlan0"
@@ -16,7 +16,7 @@ if [ ! -e "/opt/ttn-gateway/packet_forwarder/lora_pkt_fwd/local_conf.json" ]; th
         fi
         GATEWAY_EUI=$(ip link show $GATEWAY_EUI_NIC | awk '/ether/ {print $2}' | awk -F\: '{print $1$2$3"FFFE"$4$5$6}')
         GATEWAY_EUI=${GATEWAY_EUI^^}
-	LOCAL_CONFIG_FILE=/opt/ttn-gateway/packet_forwarder/lora_pkt_fwd/local_conf.json
+	LOCAL_CONFIG_FILE=/opt/lora-gateway/packet_forwarder/lora_pkt_fwd/local_conf.json
 	echo -e "{\n\t\"gateway_conf\": {\n\t\t\"gateway_ID\": \"$GATEWAY_EUI\" \n\t}\n}" >$LOCAL_CONFIG_FILE
 	echo "$GATEWAY_EUI"
 fi
